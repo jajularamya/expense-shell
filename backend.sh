ac43ef728ee1372efea8b791f4da4c14048e124f
jajularamya <ramyajajula111@gmail.com>
2025-01-28 00:01:14 +0530

    expense-shell

---
+++ backend.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/expense-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE(){
+     if [ $1 -ne 0 ]
+then
+   echo -e "$2... $R failure $N"
+   exit 1
+else
+   echo -e "$2... $G success $N"
+fi
+}
+
+CHECK_ROOT(){
+    
+if [ $USERID -ne 0 ]
+then
+ echo "ERROR:: you must have sudo access to execute this script"
+ exit 1
+fi
+}
+
+echo "script started excecuting at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+CHECK_ROOT
+
+dnf module disable nodejs -y &>>$LOG_FILE_NAME
+VALIDATE $? "disabling existing default nodejs"
+
+dnf module enable nodejs:20 -y &>>$LOG_FILE_NAME
+VALIDATE $? "enabling nodejs 20"
+
+dnf install nodejs -y &>>$LOG_FILE_NAME
+VALIDATE $? "installing nodejs"
+
+useradd expense &>>$LOG_FILE_NAME
+VALIDATE $? "adding expense user"
+
+mkdir /app &>>$LOG_FILE_NAME
+VALIDATE $? "creating app directory"
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip
+VALIDATE $? "downloading backend"
+
+cd /app
+
+unzip /tmp/backend.zip &>>$LOG_FILE_NAME
+VALIDATE $? "unzip backend"
+
+npm install &>>$LOG_FILE_NAME
+VALIDATE $? "installing dependencies"
+
+cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service
+
+#prepare mysql schema
+
+dnf install mysql -y &>>$LOG_FILE_NAME
+VALIDATE $? "installing mysql client"
+
+mysql -h mysql.ramyaaws.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE_NAME
+VALIDATE $? "setting up the transactions schema and tables"
+
+systemctl daemon-reload &>>$LOG_FILE_NAME
+VALIDATE $? "demon reload"
+
+systemctl enable backend &>>$LOG_FILE_NAME
+VALIDATE $? "enabling backend"
+
+systemctl start backend &>>$LOG_FILE_NAME
+VALIDATE $? "starting backend"
